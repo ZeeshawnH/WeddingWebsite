224ba1f91d515879d602f417ec663770e45c3c6a
Zeeshawn Hasnain <zahasnai@ncsu.edu>
2023-06-08 17:07:29 -0400

    added theKnot inspo website

---
+++ Maaheen and Muzzammil 's Wedding Website - The Knot_files/web_surveys.js
@@ -0,0 +1,75 @@
+var _sTrackingAlreadyPresent = (typeof window._svd !== 'undefined' && typeof window._svc !== 'undefined');var _svc = window._svc || {};var _svd = window._svd || {};_svc.workspaceKey = _svc.workspaceKey || '5616dd902b3ddd9d5cc22905bfae12d4';_svd.flags = _svd.flags || {"async_consumers":true};_svd.surveys = _svd.surveys || [{"id":"d271d33e8366bdc2","activated":true,"name":"The Knot Mobile Web NPS 2022","type":"WidgetSurvey","points":[{"settings":{"logic":[],"text_on_the_left":"Not likely","text_on_the_right":"Very likely","first_range_tag":"NPS-Detractor","second_range_tag":"NPS-Passive","third_range_tag":"NPS-Promoter","first_range_goto_id":null,"second_range_goto_id":null,"third_range_goto_id":null},"id":1329597,"type":"SurveyNps","answer_type":"nps","content":"How likely are you to recommend The Knot to a friend that is getting married?","description":"","max_path":1},{"settings":{"logic":[],"user_tag":"","mandatory":false,"next_survey_point_id":-1},"id":1329598,"type":"SurveyQuestion","answer_type":"text","content":"Please explain the reason for your rating.","description":"","max_path":0,"answers":[{"id":3760342,"type":null,"user_tag":null,"next_survey_point_id":null}]}],"project_id":99923,"integrations":{},"links":[{"link":"/account/","visible":false,"link_type":"contains","regexp_text_pattern":"/account/"},{"link":"/marketplace/","visible":false,"link_type":"contains","regexp_text_pattern":"/marketplace/"},{"link":"/inbox/","visible":false,"link_type":"contains","regexp_text_pattern":"/inbox/"},{"link":"/ac/","visible":false,"link_type":"contains","regexp_text_pattern":"/ac/"},{"link":"/us/","visible":false,"link_type":"contains","regexp_text_pattern":"/us/"},{"link":"/onboarding/","visible":false,"link_type":"contains","regexp_text_pattern":"/onboarding/"},{"link":"/paper/cart","visible":false,"link_type":"contains","regexp_text_pattern":"/paper/cart"},{"link":"/paper/checkout/","visible":false,"link_type":"contains","regexp_text_pattern":"/paper/checkout/"},{"link":"theknot.com","visible":true,"link_type":"simple","regexp_text_pattern":"^theknot\\.com"},{"link":"/registry/couplesearch","visible":false,"link_type":"contains","regexp_text_pattern":"/registry/couplesearch"},{"link":"/review-wedding-vendors/","visible":false,"link_type":"contains","regexp_text_pattern":"/review\\-wedding\\-vendors/"},{"link":"/registry/checkout/","visible":false,"link_type":"contains","regexp_text_pattern":"/registry/checkout/"},{"link":"/registry/store/cart","visible":false,"link_type":"contains","regexp_text_pattern":"/registry/store/cart"}],"audiences_ids":[444181],"provider":null,"theme_id":418891,"global_tags":null,"global_close_tags":null,"settings":{"has_responses":true,"appear_method":"delayed","close_icon":"bottom","close_on_urlchange":false,"survey_language":"","display_all":false,"display_delay":10,"display_not_engaged":true,"display_per":"visit","display_percentage":0.5,"display_scroll":80,"launch":{"start_at":null,"end_at":null,"responses_limit":null},"messages":{"submit_text":"Submit","tooltip":"Please answer our short survey","answer_required_text":"Answer required","dropdown_placeholder":"Type or select an option"},"overlay":"light","place_to_show":"right","recurring":true,"recurring_period":7776000,"run_minimalized":false,"run_minimalized_mobile":true,"show_close":true,"show_minimize":false,"show_progress_bar":false,"hide_footer":true,"languages":null}},{"id":"7e769e2b7500b7ba","activated":true,"name":"The Knot Desktop NPS 2022","type":"WidgetSurvey","points":[{"settings":{"text_on_the_left":"Not likely","text_on_the_right":"Very likely","first_range_tag":"NPS-Detractor","second_range_tag":"NPS-Passive","third_range_tag":"NPS-Promoter","first_range_goto_id":null,"second_range_goto_id":null,"third_range_goto_id":null,"logic":[]},"id":1329595,"type":"SurveyNps","answer_type":"nps","content":"How likely are you to recommend The Knot to a friend that is getting married?","description":"","max_path":1},{"settings":{"user_tag":"","mandatory":false,"next_survey_point_id":-1},"id":1329596,"type":"SurveyQuestion","answer_type":"text","content":"Please explain the reason for your rating.","description":"","max_path":0,"answers":[{"id":3760341,"type":null,"user_tag":null,"next_survey_point_id":null}]}],"project_id":99923,"integrations":{},"links":[{"link":"theknot.com","visible":true,"link_type":"simple","regexp_text_pattern":"^theknot\\.com"},{"link":"/registry/couplesearch","visible":false,"link_type":"contains","regexp_text_pattern":"/registry/couplesearch"},{"link":"/account/","visible":false,"link_type":"contains","regexp_text_pattern":"/account/"},{"link":"/marketplace/","visible":false,"link_type":"contains","regexp_text_pattern":"/marketplace/"},{"link":"/inbox/","visible":false,"link_type":"contains","regexp_text_pattern":"/inbox/"},{"link":"/ac/","visible":false,"link_type":"contains","regexp_text_pattern":"/ac/"},{"link":"/us/","visible":false,"link_type":"contains","regexp_text_pattern":"/us/"},{"link":"/onboarding/","visible":false,"link_type":"contains","regexp_text_pattern":"/onboarding/"},{"link":"/paper/cart","visible":false,"link_type":"contains","regexp_text_pattern":"/paper/cart"},{"link":"/paper/checkout/","visible":false,"link_type":"contains","regexp_text_pattern":"/paper/checkout/"},{"link":"/review-wedding-vendors/","visible":false,"link_type":"contains","regexp_text_pattern":"/review\\-wedding\\-vendors/"},{"link":"/registry/checkout/","visible":false,"link_type":"contains","regexp_text_pattern":"/registry/checkout/"},{"link":"/registry/store/cart","visible":false,"link_type":"contains","regexp_text_pattern":"/registry/store/cart"}],"audiences_ids":[444214],"provider":null,"theme_id":418891,"global_tags":null,"global_close_tags":null,"settings":{"launch":{"end_at":null,"start_at":null,"responses_limit":null},"overlay":"light","messages":{"tooltip":"Please answer our short survey","submit_text":"Submit","answer_required_text":"","dropdown_placeholder":""},"recurring":true,"close_icon":"bottom","show_close":true,"display_all":false,"display_per":"visit","appear_method":"delayed","display_delay":10,"has_responses":true,"place_to_show":"right","show_minimize":false,"display_scroll":80,"run_minimalized":false,"survey_language":"","recurring_period":7776000,"show_progress_bar":false,"close_on_urlchange":false,"display_percentage":0.1,"display_not_engaged":true,"run_minimalized_mobile":true,"hide_footer":true,"languages":null}},{"id":"70842311dc228cc0","activated":true,"name":"TK Savings Program Awareness","type":"WidgetSurvey","points":[{"settings":{"randomize_answers":false,"randomize_except_last":false,"mandatory_comment":false,"logic":[]},"id":1634917,"type":"SurveyQuestion","answer_type":"single","content":"How did you first hear about The Knot Savings Program?","description":"","max_path":1,"answers":[{"id":4642755,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"From The Knot itself (on the website, via email)","add_comment":false},{"id":4642756,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"On social media (Instagram, TikTok, etc.)","add_comment":false},{"id":4642757,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"On YouTube","add_comment":false},{"id":4642758,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"On TV (Hulu, on cable/news channels, etc)","add_comment":false},{"id":4642759,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Read an article about it","add_comment":false},{"id":4642862,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"From friend/family member","add_comment":false},{"id":4642863,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Via Google/search engine","add_comment":false},{"id":4642864,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Other","add_comment":false},{"id":4643049,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Don't remember","add_comment":false}]},{"settings":{"user_tag":"","mandatory":false,"next_survey_point_id":-1},"id":1641650,"type":"SurveyQuestion","answer_type":"text","content":"What aspect of the program are you most excited about?","description":"","max_path":0,"answers":[{"id":4662482,"type":null,"user_tag":null,"next_survey_point_id":null}]}],"project_id":99923,"integrations":{},"links":[{"link":"https://www.theknot.com/savings?flag=success","visible":true,"link_type":"simple","regexp_text_pattern":"^theknot\\.com/savings\\?flag=success"}],"audiences_ids":[212164],"provider":null,"theme_id":418891,"global_tags":null,"global_close_tags":null,"settings":{"has_responses":true,"appear_method":"delayed","close_icon":"bottom","close_on_urlchange":false,"survey_language":"","display_all":false,"display_delay":1,"display_not_engaged":true,"display_per":"visit","display_percentage":100,"display_scroll":80,"launch":{"start_at":null,"end_at":null,"responses_limit":500},"messages":{"submit_text":"Submit","tooltip":"Please answer our short survey","answer_required_text":"Answer required","dropdown_placeholder":"Type or select an option"},"overlay":"light","place_to_show":"right","recurring":false,"recurring_period":86400,"run_minimalized":false,"run_minimalized_mobile":true,"show_close":true,"show_minimize":false,"show_progress_bar":true,"hide_footer":true,"languages":null}}];_svd.themes = _svd.themes || [{"settings":{"company_logo":"","personalization":false,"short_text_message":"","avatar_image":""},"id":418891,"color_scheme":{"question":"#29292a","answer":"#29292a","button":"#F76C6F","background":"#ecf0f1","progress_bar":"#F76C6F"},"type":"MicroTheme"},{"settings":{"company_logo":"","personalization":false,"short_text_message":"","avatar_image":""},"id":418891,"color_scheme":{"question":"#29292a","answer":"#29292a","button":"#F76C6F","background":"#ecf0f1","progress_bar":"#F76C6F"},"type":"MicroTheme"},{"settings":{"company_logo":"","personalization":false,"short_text_message":"","avatar_image":""},"id":418891,"color_scheme":{"question":"#29292a","answer":"#29292a","button":"#F76C6F","background":"#ecf0f1","progress_bar":"#F76C6F"},"type":"MicroTheme"}];_svd.permissions = _svd.permissions || {"targeting_javascript_api":true};_svd.audiences = _svd.audiences || [{"id":212164,"filters":[]},{"id":444181,"filters":[{"exclude":false,"parameters":{"type":"Mobile"},"name":"device"},{"exclude":false,"parameters":{"count":1},"name":"visit_frequency"}]},{"id":444214,"filters":[{"exclude":false,"parameters":{"type":"Desktop"},"name":"device"},{"exclude":false,"parameters":{"count":1},"name":"visit_frequency"}]}];_svd.integrations = _svd.integrations || [];_svd.installing = _svd.installing || false;_svd.translations = _svd.translations || null;// Generated at: 2023\u002D05\u002D28\u002015\u003A33\u003A54
+(function () {
+  if (_sTrackingAlreadyPresent) {
+    return;
+  }
+
+  if (document && document.head) {
+    var linkElement = document.createElement('link');
+    linkElement.href = 'https\u003A\/\/surveys\u002Dstatic.survicate.com/fonts/fonts.css';
+    linkElement.rel = 'stylesheet';
+    linkElement.type = 'text/css';
+    document.head.appendChild(linkElement);
+  }
+
+  var isIE = window.navigator.userAgent.indexOf('MSIE') !== -1 || window.navigator.userAgent.match(/Trident.*rv\:11\./);
+  var isSafari = window.navigator.vendor && window.navigator.vendor.indexOf('Apple') > -1 && window.navigator.userAgent && window.navigator.userAgent.indexOf('CriOS') == -1 && window.navigator.userAgent.indexOf('FxiOS') == -1;
+
+  var isOldSafari = false;
+  if (isSafari && window.navigator.userAgent) {
+    var match = window.navigator.userAgent.match(/Version.([0-9]+)\./);
+    if (match && match.length === 2) {
+      if (parseInt(match[1]) < 11) {
+        isOldSafari = true;
+      }
+    }
+  }
+  var isOldEdge = window.navigator.userAgent.match(/Edge\/(15\.15|18\.18)/);
+
+  var coreUrls = ['https://surveys-static.survicate.com/widget_core-18.5.3.js'];
+  var e = document.getElementsByTagName('script')[0];
+
+  if (isIE || isOldSafari || isOldEdge) {
+    var css = 'h1 { width: 75%; height: 50%; overflow: auto; margin: auto; position: absolute; top: 0; left: 0; bottom: 0; right: 0; text-align: center; font-family: sans-serif; }'
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var style = document.createElement('style');
+    var message = 'Your browser doesn’t meet our security standards. Please update your browser to answer this survey.'
+    var body = document.querySelector('body');
+    var splash = document.createElement('div');
+    var survey = document.querySelector('.survicate-survey');
+
+    if(!head || !survey) {
+      return;
+    }
+
+    head.appendChild(style);
+
+    style.type = 'text/css';
+
+    if (style.styleSheet){
+      // This is required for IE8 and below.
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+
+    splash.classList.add('splash-screen');
+    splash.innerHTML = '<h1>' + message + '</h1>';
+
+    if(!body) {
+      return;
+    }
+
+    body.appendChild(splash);
+
+    return;
+  }
+
+  for (var i = 0; i < coreUrls.length; i++) {
+    var s = document.createElement('script');
+    s.setAttribute('crossorigin', 'anonymous');
+    s.src = coreUrls[i];
+    s.async = true;
+    e.parentNode.insertBefore(s, e);
+  }
+})();
